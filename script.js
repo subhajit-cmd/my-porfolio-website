77dfeac11fee1527b0af1298eab2c37a260d9fcd
Subhajit Sarkar <subhajit.sarkar0407@gmail.com>
2025-07-29 22:31:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+  function viewCV() {
+    // Replace with your actual path to the PDF
+    const cvUrl = 'C:\Users\subho\Downloads\Favorites\Downloads\Documents\Desktop\New Me\Project (html+css)\my Porfolio site (HTML+CSS+Js)\assets\Subhajit Sarkar cv.pdf' 
+    window.open(cvUrl, '_blank'); // Opens in new tab
+  }
