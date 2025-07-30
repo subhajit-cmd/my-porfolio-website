6f0cdc19dc59e9c588d15bff7f2b10a1ab6fc615
Subhajit Sarkar <subhajit.sarkar0407@gmail.com>
2025-07-30 11:43:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function viewCV() {
-        window.open('files/your-cv.pdf', '_blank');
-    }
+  function viewCV() {
+          window.open('files/your-cv.pdf', '_blank');
+      }
 
-    const toggle = document.getElementById("toggleMode");
-    toggle.addEventListener("click", () => {
-        document.body.classList.toggle("dark-mode");
-        toggle.textContent = document.body.classList.contains("dark-mode") ? "☀️ Light Mode" : "🌙 Dark Mode";
-    });
+      const toggle = document.getElementById("toggleMode");
+      toggle.addEventListener("click", () => {
+          document.body.classList.toggle("dark-mode");
+          toggle.textContent = document.body.classList.contains("dark-mode") ? "☀️ Light Mode" : "🌙 Dark Mode";
+      });
